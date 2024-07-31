7b163a137b8b90518fe4bceb6b0f87a604ed96fb
DanielGabGon <dan.gabito@gmail.com>
2024-07-31 16:25:25 -0600

    Logro: Creación de entidades y sus relaciones -Entidad Movie -Entidad UserMovie -Entidad Rating

---
+++ src/main/java/com/dangabito/projects/MovieManagement/persistence/entity/Movie.java
@@ -0,0 +1,81 @@
+package com.dangabito.projects.MovieManagement.persistence.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Movie {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	private String director;
+
+	private String genre;
+
+	private String releaseYear;
+
+	@OneToMany(targetEntity = Rating.class, fetch = FetchType.EAGER, mappedBy = "movie")
+	private List<Rating> ratings;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getReleaseYear() {
+		return releaseYear;
+	}
+
+	public void setReleaseYear(String releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+
+	public List<Rating> getRatings() {
+		return ratings;
+	}
+
+	public void setRatings(List<Rating> ratings) {
+		this.ratings = ratings;
+	}
+
+}
---
+++ src/main/java/com/dangabito/projects/MovieManagement/persistence/entity/Rating.java
@@ -0,0 +1,86 @@
+package com.dangabito.projects.MovieManagement.persistence.entity;
+
+import org.hibernate.annotations.Check;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Rating {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "movie_id", nullable = false)
+	private Long movieId;
+
+	@Column(name = "user_id", nullable = false)
+	private Long userId;
+
+	@Check(constraints = "rating>=0 and rating <=5")
+	@Column(nullable = false)
+	private int rating;
+
+	@ManyToOne
+	@JoinColumn(name = "movie_id", insertable = false, updatable = false)
+	private Movie movie;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id", insertable = false, updatable = false)
+	private UserMovie userMovie;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(Long movieId) {
+		this.movieId = movieId;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	public UserMovie getUser() {
+		return userMovie;
+	}
+
+	public void setUser(UserMovie userMovie) {
+		this.userMovie = userMovie;
+	}
+
+}
